5a77a124342b397886c71cdd0066b42c670da6da
Olwanishola <lukusope@yahoo.com>
2022-09-12 08:49:41 -0700

    models/state.py

---
+++ models/state.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+'''
+    Implementation of the State class
+'''
+
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.city import City
+import os
+import models
+
+
+class State(BaseModel, Base):
+    '''
+        Implementation for the State.
+    '''
+    __tablename__ = 'states'
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", passive_deletes=True, backref="state")
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            """
+            """
+            cities_dict = models.storage.all('City')
+            cities_list = []
+            for city in cities_dict.values():
+                if city.state_id == self.id:
+                    cities_list.append(city)
+            return cities_list
+
