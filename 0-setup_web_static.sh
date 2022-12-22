dba3ae55c53edee75aadba1912e12ca5a1c837af
thebenjaminibe <frankynoble@gmail.com>
2022-12-22 14:04:16 -0800

    commit

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
-# script that sets up your web servers for the deployment of web_static
-sudo apt-get -y update
-sudo apt-get -y install nginx
-mkdir -p /data/web_static/shared/
+# Setup a web servers for the deployment of web_static.
+apt update -y
+apt install -y nginx
 mkdir -p /data/web_static/releases/test/
-touch /data/web_static/releases/test/index.html
-ln -fs /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data/
-echo "<html><head></head><body>Success</body></html>" > /data/web_static/releases/test/index.html
-sed -i "s/# Only/\n\tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current\/; \n\t\
-\tautoindex off;\n\t}\n \n\t# Only/" /etc/nginx/sites-available/default
+mkdir -p /data/web_static/shared/
+echo "<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    <p>Nginx server test</p>
+  </body>
+</html>" | tee /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data
+sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
 sudo service nginx restart
